8bb277ede449cb17a904286628be086f5fc48bec
Robert Oliveira <robertsudoliveira@gmail.com>
2021-12-15 19:07:18 -0300

    JavaScript

---
+++ main.js
@@ -0,0 +1,9 @@
+const nome = "Robert ";
+const sobrenome = "Oliveira";
+const idade = 26;
+const peso = 86;
+const altura = 1.76;
+const AnoDeNascimento = 1995;
+
+let imc = peso / (altura * altura);
+console.log(imc)
