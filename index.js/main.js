6f4d873b6c1772d72a0594896472e1067073ae09
Robert Oliveira <robertsudoliveira@gmail.com>
2021-12-17 11:34:31 -0300

    exercicio resolvido de js

---
+++ index.js/main.js
@@ -0,0 +1,11 @@
+let numero1 = prompt("digite o primeiro valor");
+let numero2 = prompt("digite o segundo  valor");
+
+numero1 = Number(numero1);
+numero2 = Number(numero2);
+
+const resultado = (numero1 + numero2);
+
+alert("seu resultado é " +  resultado);
+
+
