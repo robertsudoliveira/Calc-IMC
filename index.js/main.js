d856bd7a75d82364019eee65f9747c0afe24e63c
Robert Oliveira <robertsudoliveira@gmail.com>
2021-12-17 22:38:40 -0300

    calculo do imc feito sozinho

--- index.js/main.js
+++ index.js/main.js
@@ -1,11 +1,12 @@
-let numero1 = prompt("digite o primeiro valor");
-let numero2 = prompt("digite o segundo  valor");
-
-numero1 = Number(numero1);
-numero2 = Number(numero2);
-
-const resultado = (numero1 + numero2);
-
-alert("seu resultado é " +  resultado);
 
 
+let nome = prompt("digite aqui o seu nome ");
+let sobreNome = prompt("digite aqui o seu Sobrenome ");
+let peso = prompt("Qual é o seu peso atual ?");
+let altura = prompt("Qual é a sua altura ?");
+peso = parseFloat(peso);
+altura = parseFloat(altura);
+const  resultado = peso / (altura * altura);
+
+alert(`seu nome é ${nome} e o sobrenome é ${sobreNome} essa é a sua altura ${altura} e esse é o seu peso ${peso} 
+o resultado do seu imc é ${resultado}`);
